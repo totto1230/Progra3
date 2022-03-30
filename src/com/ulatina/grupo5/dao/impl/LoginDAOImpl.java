4bc4e6e6fd7c9398f579cb2f5ba8cd7eee571637
Creative App <fernando.loria@live.com>
2022-03-30 17:12:51 -0600

    cambios mayores

---
+++ src/com/ulatina/grupo5/dao/impl/LoginDAOImpl.java
@@ -0,0 +1,202 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ulatina.grupo5.dao.impl;
+
+import com.ulatina.grupo5.dao.BaseDAO;
+import com.ulatina.grupo5.helpers.Conexion;
+import com.ulatina.grupo5.modelo.Login;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author fernando
+ */
+public class LoginDAOImpl implements BaseDAO{
+
+    Conexion conectar = new Conexion();
+
+    PreparedStatement ps;
+    ResultSet rs;
+    Connection con;
+    
+    Login p = new Login();
+
+    @Override
+    public int nextID() {
+       String sql = "select COALESCE(max(idLogin),0) + 1 as nextCode from Login";
+       Integer nextCode = 0;
+       try {
+           con = conectar.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while (rs.next()){
+                nextCode = Integer.parseInt(rs.getString("nextCode"));
+            }
+            con.close(); 
+       }
+       catch (SQLException e) {
+            System.out.println("Error");
+            return -1;
+        }
+       return nextCode;  
+    }
+    
+    @Override
+    public Boolean insertar(Object obj) {
+        p = (Login) obj;
+        String sql = "INSERT INTO Login (id,numTickets,fechaLogin,fechaLogoff) VALUES (?,?,?,?);";
+        try {
+            conectar.connectar();
+            con = conectar.getConnection();
+            ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, p.getIdLogin());
+            ps.setInt(2,p.getNumTickets());
+            ps.setDate(3, (Date)p.getFechaLogin());
+            ps.setDate(4, (Date)p.getFechaLogoff());
+
+            int registros = ps.executeUpdate();
+
+            if (registros > 0) {
+                con.close();
+                return true;
+            } else {
+                con.close();
+                return false;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error");
+            return false;
+        }
+    }
+
+   @Override
+    public Boolean actualizar(Object obj) {
+        
+        p = (Login) obj;
+        
+        String sql = "UPDATE Login SET numTickets = ?, fechaLogin= ?, fechaLogoff = ? WHERE id= ?";
+        try {
+            
+            conectar.connectar();
+            
+            con = conectar.getConnection();
+            ps = con.prepareStatement(sql);
+
+            ps.setInt(1,p.getNumTickets());
+            ps.setDate(2, (Date)p.getFechaLogin());
+            ps.setDate(3, (Date)p.getFechaLogoff());
+            ps.setInt(4, p.getIdLogin());
+
+           
+            int registros = ps.executeUpdate();
+            
+            if(registros > 0){
+                con.close();
+                return true;
+            }
+            else {
+                con.close();
+                return false;
+            }
+            
+        } catch (SQLException e) {
+            System.out.println("Error");
+            return false;
+        }
+    }
+
+
+    @Override
+    public Boolean eliminar(Object obj) {
+        
+        p = (Login) obj;
+        
+        String sql = "DELETE FROM Login WHERE WHERE id= ?";
+        
+        try {
+            
+            conectar.connectar();
+            con = conectar.getConnection();
+            ps = con.prepareStatement(sql);
+            
+            ps.setInt(1,p.getIdLogin());
+            
+            int registros = ps.executeUpdate();
+            
+            if(registros > 0){
+                con.close();
+                return true;
+            }
+            else {
+                con.close();
+                return false;
+            }
+            
+        } catch (SQLException e) {
+            
+            return false;
+        }
+       
+    }
+    
+    @Override
+    public Boolean eliminarTodos(Integer id) {
+        return null;
+    }
+    
+    @Override
+    public void listar(JTable table) {
+        
+        String[] titulos = {"ID", "Ticketes", "Fecha Login" , "Fecha Logogg"};
+        String[] registros = new String[titulos.length];
+        DefaultTableModel model = new DefaultTableModel(null, titulos);
+        
+        String sql = "SELECT id, numTickets, fechaLogin, fechaLogoff FROM Login";
+        
+        try {
+            con = conectar.getConnection();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while (rs.next()){
+            
+                registros[1] = rs.getString("id");
+                registros[2] = rs.getString("numTickets");
+                registros[3] = rs.getString("fechaLogin");
+                registros[4] = rs.getString("fechaLogoff");
+                model.addRow(registros);
+    
+            }
+            table.setModel(model);
+            con.close();   
+        }
+        catch (Exception ex){
+            System.out.println("Error");
+        }
+    }
+   
+
+    @Override
+    public Object listarUno(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Object[] listarPor(Object obj) {
+        return null;
+    }
+    
+    
+}
+
