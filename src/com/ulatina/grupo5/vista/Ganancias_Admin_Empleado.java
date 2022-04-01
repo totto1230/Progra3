2b76acfb3423c4c4391ae7a14418146e8ecf7ffb
josep <josep@DESKTOP-D7M95JK>
2022-03-31 19:29:13 -0600

    Vista Ganancias & Controller

--- src/com/ulatina/grupo5/vista/Ganancias_Admin_Empleado.java
+++ src/com/ulatina/grupo5/vista/Ganancias_Admin_Empleado.java
@@ -26,10 +26,9 @@ public class Ganancias_Admin_Empleado extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tbkAtracciones = new javax.swing.JTable();
+        tbkAtraccionesGanacias = new javax.swing.JTable();
         dateChFechaInicio = new com.toedter.calendar.JDateChooser();
         dateChFechaFinal = new com.toedter.calendar.JDateChooser();
         btnPreviousGanacias = new javax.swing.JButton();
@@ -43,7 +42,7 @@ public class Ganancias_Admin_Empleado extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        tbkAtracciones.setModel(new javax.swing.table.DefaultTableModel(
+        tbkAtraccionesGanacias.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null},
                 {null, null, null, null, null},
@@ -62,7 +61,7 @@ public class Ganancias_Admin_Empleado extends javax.swing.JFrame {
                 return types [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(tbkAtracciones);
+        jScrollPane1.setViewportView(tbkAtraccionesGanacias);
 
         btnPreviousGanacias.setIcon(new javax.swing.ImageIcon(getClass().getResource("/proyecto/images/previous1.png"))); // NOI18N
         btnPreviousGanacias.setContentAreaFilled(false);
@@ -159,30 +158,21 @@ public class Ganancias_Admin_Empleado extends javax.swing.JFrame {
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 641, javax.swing.GroupLayout.PREFERRED_SIZE))))
         );
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel4, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 1, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -230,18 +220,17 @@ public class Ganancias_Admin_Empleado extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel JLabelFechaInicio;
-    private javax.swing.JButton btnFiltrar;
-    private javax.swing.JButton btnPreviousGanacias;
-    private javax.swing.JCheckBox chkFiltroFechas;
-    private com.toedter.calendar.JDateChooser dateChFechaFinal;
-    private com.toedter.calendar.JDateChooser dateChFechaInicio;
-    private javax.swing.JComboBox<String> ddlFiltroSec;
+    public javax.swing.JButton btnFiltrar;
+    public javax.swing.JButton btnPreviousGanacias;
+    public javax.swing.JCheckBox chkFiltroFechas;
+    public com.toedter.calendar.JDateChooser dateChFechaFinal;
+    public com.toedter.calendar.JDateChooser dateChFechaInicio;
+    public javax.swing.JComboBox<String> ddlFiltroSec;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel4;
+    public javax.swing.JPanel jPanel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel jlblSeccion;
     private javax.swing.JLabel lblFechaInicio;
-    private javax.swing.JTable tbkAtracciones;
+    public javax.swing.JTable tbkAtraccionesGanacias;
     // End of variables declaration//GEN-END:variables
 }
