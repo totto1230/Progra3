486e74689c3e05ff4a24efc697e2db9cba9c88ff
Creative App <fernando.loria@live.com>
2022-04-01 20:09:53 -0600

    Ganancias

--- src/com/ulatina/grupo5/controlador/SeguimientoController.java
+++ src/com/ulatina/grupo5/controlador/SeguimientoController.java
@@ -2,16 +2,15 @@
 package com.ulatina.grupo5.controlador;
 
 import com.ulatina.grupo5.dao.BaseDAO;
-import com.ulatina.grupo5.dao.impl.AtraccionesDAOImpl;
+import com.ulatina.grupo5.dao.impl.MantenimientoDAOImpl;
 import com.ulatina.grupo5.modelo.Atracciones;
-import com.ulatina.grupo5.vista.Registro_Atracciones_Admin;
 import com.ulatina.grupo5.vista.Seguimiento_De_Atracciones_Admin;
 
 
 
 public class SeguimientoController {
     Atracciones atraccion = new Atracciones();
-    BaseDAO dao = new AtraccionesDAOImpl();
+    BaseDAO dao = new MantenimientoDAOImpl();
     Seguimiento_De_Atracciones_Admin vista = new Seguimiento_De_Atracciones_Admin();
     
     public SeguimientoController(Seguimiento_De_Atracciones_Admin vista) {
